871db5a7a7d85bcc6385f159d4d0bcc33e817410
Maria4E <maria.einman@gmail.com>
2021-12-13 10:13:24 +0100

    updated search tests

--- src/test/java/eql/logicaldoc/LogicaldocTest.java
+++ src/test/java/eql/logicaldoc/LogicaldocTest.java
@@ -21,13 +21,42 @@ public class LogicaldocTest {
 	
 	Logger log = Logger.getLogger(LogicaldocTest.class.getName());
 	
+	// Implémentation "intermédiaire"
+	// (https://richard-carree.developpez.com/tutoriels/junit-services-web-soapui/)
+	
+	@Test
+    public void LogicalDocTest_Bookmarks()
+            throws XmlException, IOException, SoapUIException {
+		log.info("Running LOGICALDOC Bookmarks APIs tests");
+        // Créer une nouvelle instance de WsdlProject en spécifiant le chemin absolu du projet SoapUI
+        WsdlProject project = new WsdlProject(
+                "src/test/resources/LOGICALDOC-BOOKMARKS-TEST-soapui-project.xml");
+        // Récupère tous les TestSuites inclus dans le projet SoapUI
+        List<TestSuite> testSuiteList = project.getTestSuiteList();
+        // Itération sur tous les TestSuites du projet
+        for (TestSuite ts : testSuiteList) {
+            log.info("******Running " + ts.getName() + "***********");
+            // Récupère tous les TestCases d'une TestSuite
+            List<TestCase> testCaseList = ts.getTestCaseList();
+            // Itération sur tous les TestCases d'un TestSuite particulier
+            for (TestCase testcase : testCaseList) {
+                log.info("******Running " + testcase.getName() + "***********");
+                // Exécute le TestCase spécifié
+                TestRunner testCaseRunner = testcase.run(new PropertiesMap(), false);
+                // Vérifie si le TestCase s'est terminé correctement
+                // ou s'il a échoué à cause d'un échec d'assertion
+                assertEquals(TestRunner.Status.FINISHED, testCaseRunner.getStatus());
+            }
+        }
+    }
+	
 	@Test
     public void LogicalDocTest_Docs_And_Search()
             throws XmlException, IOException, SoapUIException {
 		log.info("Running LOGICALDOC Documents and Search APIs tests");
         // Créer une nouvelle instance de WsdlProject en spécifiant le chemin absolu du projet SoapUI
         WsdlProject project = new WsdlProject(
-                "src/test/resources/JN_Logicaldoc-soapui-project.xml");
+                "src/test/resources/UPDATED_JN_Logicaldoc-soapui-project.xml");
         // Récupère tous les TestSuites inclus dans le projet SoapUI
         List<TestSuite> testSuiteList = project.getTestSuiteList();
         // Itération sur tous les TestSuites du projet
@@ -73,29 +102,5 @@ public class LogicaldocTest {
         }
     }
 	
-	@Test
-    public void LogicalDocTest_Bookmarks()
-            throws XmlException, IOException, SoapUIException {
-		log.info("Running LOGICALDOC Bookmarks APIs tests");
-        // Créer une nouvelle instance de WsdlProject en spécifiant le chemin absolu du projet SoapUI
-        WsdlProject project = new WsdlProject(
-                "src/test/resources/LOGICALDOC-BOOKMARKS-TEST-soapui-project.xml");
-        // Récupère tous les TestSuites inclus dans le projet SoapUI
-        List<TestSuite> testSuiteList = project.getTestSuiteList();
-        // Itération sur tous les TestSuites du projet
-        for (TestSuite ts : testSuiteList) {
-            log.info("******Running " + ts.getName() + "***********");
-            // Récupère tous les TestCases d'une TestSuite
-            List<TestCase> testCaseList = ts.getTestCaseList();
-            // Itération sur tous les TestCases d'un TestSuite particulier
-            for (TestCase testcase : testCaseList) {
-                log.info("******Running " + testcase.getName() + "***********");
-                // Exécute le TestCase spécifié
-                TestRunner testCaseRunner = testcase.run(new PropertiesMap(), false);
-                // Vérifie si le TestCase s'est terminé correctement
-                // ou s'il a échoué à cause d'un échec d'assertion
-                assertEquals(TestRunner.Status.FINISHED, testCaseRunner.getStatus());
-            }
-        }
-    }
+	
 }
